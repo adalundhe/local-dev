dbd274420d3a2184fe0e7c07d0796edde78d1375
Ada Lundhe <ada@datavant.com>
2024-02-04 12:07:29 -0600

    AL: Update download script

--- setup.sh
+++ setup.sh
@@ -1,20 +1,41 @@
 #!/usr/bin/env bash
 
-USER_PASSWORD=${USER_PASSWORD:-}
-NIX_CONFIG=${NIX_CONFIG:-"/etc/nix/nix.conf"}
+#!/usr/bin/env bash
+set -euo pipefail
 
+cat <<'EOF'
+Welcome to the setup of your awesome local dev environment!
+This script will:
+1. Install nix if necessary
+2. Print out the commands required to get started
+3. Send you on your merry way!
+EOF
 
-curl -L https://nixos.org/nix/install | sh -s -- --daemon --yes
+[[ ! -d /nix/store ]] && {
+  printf '\n\n%s\n\n' "Installing Nix!"
+  curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
+}
 
-if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+if ! command -v nix >/dev/null && [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+  printf '\n\n%s\n\n' "Adding nix into the script's environment"
   . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
 fi
 
-# Testing whether Nix is available in subsequent commands
-nix --version
+printf '\n\n%s: ... %s\n' "Testing whether Nix is available in subsequent commands" "$(nix --version)"
 
-echo "build-users-group = nixbld
-experimental-features = nix-command flakes
-" | sudo tee $NIX_CONFIG > /dev/null
+nix develop "github:scorbettUM/local-dev"
 
-NIXPKGS_ALLOW_UNFREE=1 nix develop "github:scorbettUM/local-dev"
+cat <<'EOF'
+
+Nice! Check it out, you've got so much fun stuff available now, including:
+
+- just
+- direnv
+- docker
+- docker-compose
+- Python 3
+- NodeJS LTS
+- Ripgrep
+- Devcontainers CLI
+
+EOF
